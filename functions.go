8dfd2da576f21f4a77c50639b7ef4fe7f1e77089
Anthony Alaribe <anthonyalaribe@ymail.com>
2015-02-08 15:23:55 +0000

    update user function added

---
+++ functions.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+//NewUser is for adding a new user to the database. Please note that what you pass to the function is a pointer to the actual data, note the data its self. ie newUser(&NameofVariable)
+func NewUser(data *User) error {
+
+	//MONGOSERVER is a variable containing the mongo db instance address
+	session, err := mgo.Dial(MONGOSERVER)
+	checkPanic(err)
+	defer session.Close()
+
+	//MONGODB is the database name while MONGOC is the collection name
+	collection := session.DB(MONGODB).C("users")
+
+	err = collection.Insert(data)
+
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+//UpdateUser updates a users details
+func UpdateUser(data *User) error {
+	session, err := mgo.Dial(MONGOSERVER)
+
+	if err != nil {
+		return err
+	}
+
+	defer session.Close()
+
+	collection := session.DB(MONGODB).C("users")
+
+	err = collection.UpdateId(bson.ObjectIdHex(data.ID), data)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//AddSkill adds a skill to the collection
+func AddSkill(data *Skill) error {
+	session, err := mgo.Dial(MONGOSERVER)
+
+	if err != nil {
+		return err
+	}
+
+	defer session.Close()
+
+	skillCollection := session.DB(MONGODB).C("skills")
+
+	err = skillCollection.Insert(data)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//GetSkills gets the list of skills from a user with a particular ID
+func GetSkills(id string) (Skill, error) {
+	session, err := mgo.Dial(MONGOSERVER)
+
+	result := Skill{}
+
+	if err != nil {
+		return result, err
+	}
+
+	defer session.Close()
+
+	skillCollection := session.DB(MONGODB).C("skills")
+	err = skillCollection.Find(bson.M{"userID": id}).Select(bson.M{"comments": 0}).One(&result)
+	checkFmt(err)
+	return result, nil
+
+}
+
+//AddComment adds a comment to a skill
+func AddComment(comment *Comment, id string) error {
+	session, err := mgo.Dial(MONGOSERVER)
+
+	if err != nil {
+		return err
+	}
+
+	defer session.Close()
+
+	skillCollection := session.DB(MONGODB).C("skills")
+
+	err = skillCollection.UpdateId(
+		bson.ObjectIdHex(id),
+		bson.M{
+			"$push": bson.M{"Comments": comment},
+		})
+
+	return nil
+
+}
+
+//checkFmt checks the value of an error and prints it to standard output. I'm
+//adding it to reduce the number of error checking ifs in my code
+
+func checkFmt(err error) {
+	if err != nil {
+		fmt.Println(err.Error)
+	}
+}
+
+//checkPanic checks the value of an error and panics if it isnt empty, thereby
+//closing the program.
+func checkPanic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
-
-	"gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
 )
 
 var (
@@ -14,29 +13,10 @@ var (
 
 	//MONGODB stores the name of the database instance
 	MONGODB string
+
+	//PORT stores the port number
+	PORT string
 )
-
-//User would hold the user data for retrieving and sending items to the database
-type User struct {
-	Name string
-	ID   string
-    About string
-    Email string
-    Location string
-    Address string
-    Phone string
-}
-
-type Skill struct {
-    Skill_name string
-    User_id int
-    Location string
-    Address string
-    Price string
-    Tag_name string
-    Description string
-    
-    }
 
 func init() {
 	MONGOSERVER = os.Getenv("MONGOSERVER")
@@ -51,73 +31,19 @@ func init() {
 		MONGODB = "oddjobs"
 	}
 
-}
+	PORT = os.Getenv("PORT")
+	if PORT == "" {
+		fmt.Println("No Global port has been defined, using default")
 
-//NewUser is for adding a new user to the database. Please note that what you pass to the function is a pointer to the actual data, note the data its self. ie newUser(&NameofVariable)
-func NewUser(data *User) error {
+		PORT = ":8080"
 
-	//MONGOSERVER is a variable containing the mongo db instance address
-	session, err := mgo.Dial(MONGOSERVER)
-	checkPanic(err)
-	defer session.Close()
-
-	//MONGODB is the database name while MONGOC is the collection name
-	collection := session.DB(MONGODB).C("users")
-
-	err = collection.Insert(data)
-
-	if err != nil {
-		return err
 	}
-	return nil
 
-}
-
-func AddSkill(data *Skill) error{
-    session, err := mgo.Dial(MONGOSERVER)
-    checkPanic(err)
-    defer session.Close()
-    
-    skill_collection := session.DB(MONGODB).C("skills")
-    
-    err = skill_collection.Insert(data)
-    
-    if err != nil {
-        return err
-    }
-    return nil
-}
-
-func GetSkills(id string) Skill{
-    session, err := mgo.Dial(MONGOSERVER)
-    checkPanic(err)
-    defer session.Close()
-    
-    skill_collection := session.DB(MONGODB).C("skills")
-    
-    result := Skill{}
-    err = skill_collection.Find(bson.M{"User_id": id}).One(&result)
-    checkFmt(err)
-return result
-
-}
-//checkFmt checks the value of an error and prints it to standard output. I'm
-//adding it to reduce the number of error checking ifs in my code
-
-func checkFmt(err error) {
-	if err != nil {
-		fmt.Println(err.Error)
-	}
-}
-
-//checkPanic checks the value of an error and panics if it isnt empty, thereby
-//closing the program.
-func checkPanic(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
 
 func main() {
+	http.HandleFunc("/", HomeHandler)
+
+	log.Fatal(http.ListenAndServe(PORT, nil))
 
 }
